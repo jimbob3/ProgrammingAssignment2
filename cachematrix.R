68b4e0fc0d760bed830b1c48ff0843c932cbb77a
jimbob3 <tobybarrus@yahoo.com>
2015-01-22 16:20:02 -0700

    Inserted the functions and comments I created for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions work together to cache the inverse of a
+## matrix to the parent environment of the makeCacheMatrix function.
+## The example functions makeVector and cachemean from the 
+## Assignment 2 web page were used heavily for inspiration to 
+## create makeCacehMatrix and cacheSolve.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of objects stored in the environment
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+    minv <- NULL
+    set <- function(y){
+          x <<- y
+          minv <<- NULL
+    }
+    get <- function()x
+    setminv <- function(solve) minv <<- solve
+    getminv <- function()minv
+    list(set=set, get=get, setminv=setminv, getminv=getminv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks to see if the matrix inverse already exists.
+## If it does, then it just returns the inverse of the matrix stored in the environment.
+## If the inverted matrix does not exist, it calculates the inverse of the matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  minv <- x$getminv()
+  if(!is.null(minv)){
+      message("getting cached matrix inverse")
+      return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setminv(minv)
+  minv
 }
